f500fe576950f0cb7fe79f8a033d2157572695d0
Aliaksei Mironau <dementr2008@gmail.com>
2024-02-10 18:05:51 +0300

    feat:add fix

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function getTimeOfDay(hour) {
-    if (hour < 6) return "Доброй ночи!";
-    if (hour < 12) return "Доброе утро!";
-    if (hour < 18) return "Добрый день!";
-    return "Добрый вечер!";
+    if (hour < 6) return "Доброй ночи";
+    if (hour < 12) return "Доброе утро";
+    if (hour < 18) return "Добрый день";
+    return "Добрый вечер";
 }
 
-function updateGreeting() {
+function showTimeOfDay() {
+    const username = document.getElementById("username").value;
     const currentTime = new Date();
     const currentHour = currentTime.getHours();
     const timeOfDayMessage = getTimeOfDay(currentHour);
-    document.getElementById("timeOfDay").textContent = timeOfDayMessage;
+    document.getElementById("timeOfDay").innerText = `${timeOfDayMessage}, ${username}!`;
 }
-
-updateGreeting();
