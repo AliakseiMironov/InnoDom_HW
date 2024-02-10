26b3e61766052acfbfc6c2a475f037db10d6bce5
Aliaksei Mironau <dementr2008@gmail.com>
2024-02-10 15:21:30 +0300

    feat:add HW_24

---
+++ script.js
@@ -0,0 +1,15 @@
+function getTimeOfDay(hour) {
+    if (hour < 6) return "Доброй ночи!";
+    if (hour < 12) return "Доброе утро!";
+    if (hour < 18) return "Добрый день!";
+    return "Добрый вечер!";
+}
+
+function updateGreeting() {
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+    const timeOfDayMessage = getTimeOfDay(currentHour);
+    document.getElementById("timeOfDay").textContent = timeOfDayMessage;
+}
+
+updateGreeting();
